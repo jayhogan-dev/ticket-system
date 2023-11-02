c5439bf24de564bce1e2962da32ad84faf21c5d5
Jay Hogan <jayhogan.dev@gmail.com>
2023-11-02 16:19:09 -0700

    navbar added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,22 @@
+import Link from 'next/link';
+import { FaTicketAlt, FaHome } from 'react-icons/fa';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex justify-between bg-nav p-4">
+      <div className="flex items-center space-x-8">
+        <Link href="/">
+          <FaHome className="icon" />
+        </Link>
+        <Link href="/ticket-page/new">
+          <FaTicketAlt className="icon" />
+        </Link>
+      </div>
+      <div>
+        <p className="text-default-text">jay@gmail.com</p>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
