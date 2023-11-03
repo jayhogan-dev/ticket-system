eb33a5c1d08242317b8d0dbae93eb1a50c078d33
Jay Hogan <jayhogan.dev@gmail.com>
2023-11-02 18:50:42 -0700

    form created

--- app/ticket-page/[id]/page.jsx
+++ app/ticket-page/[id]/page.jsx
@@ -1,5 +1,7 @@
+import TicketForm from '@/components/TicketForm';
+
 const TicketPage = ({ params }) => {
-  return <div>TicketPage {params.id}</div>;
+  return <TicketForm />;
 };
 
 export default TicketPage;
